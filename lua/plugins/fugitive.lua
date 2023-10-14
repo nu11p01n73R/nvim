eedca566d50ef3f3a8382f5a57919dd89a870dbf
nu11p01n73R <kjjohny1991@gmail.com>
2023-10-15 02:10:36 +0530

    Add keymap for git push

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -5,6 +5,7 @@ return {
         vim.keymap.set("n", "<leader>gs", function() vim.cmd("vertical G") end)
         vim.keymap.set("n", "<leader>gd", function() vim.cmd("Gvdiffsplit!") end)
         vim.keymap.set("n", "<leader>gbl", function() vim.cmd("G blame") end)
+        vim.keymap.set("n", "<leader>gP", function() vim.cmd("G push") end)
 
         -- Populate command line
         vim.keymap.set("n", "<leader>gp", ":G pull")
